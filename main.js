f46999d0cb510cb275ff78bcc8a08b94c35a8078
TroadecJb <110687346+TroadecJb@users.noreply.github.com>
2023-02-11 23:10:33 +0100

    Carrousel Structure

--- main.js
+++ main.js
@@ -8,17 +8,18 @@ async function fetchBest() {
     const bestTitle = document.getElementById("best-title");
     const bestCover = document.getElementsByClassName('best-cover')[0].getElementsByTagName('img')[0];
     const bestDescrip = document.getElementById("top-desc");
+    const bestModalBtn = document.getElementsByClassName("best-modalBtn")[0];
     // récupération des informations
     const response = await fetch(mainURL + "?sort_by=-imdb_score");
     const data = await response.json();
     const responseBestDescrip = await fetch(data["results"][0]["url"]);
     const dataBestDescrip = await responseBestDescrip.json();
     // assignation du contenu aux éléments 
+    bestModalBtn.setAttribute("onclick", "openModal()");
     bestTitle.innerText = data["results"][0]["title"];
     bestCover.src = data["results"][0]["image_url"];
     bestDescrip.innerText = dataBestDescrip["description"];
 }
-
 
 async function fetchCategories(name) {
     const response = await fetch(mainURL + "?sort_by=-imdb_score&genre=" + name);
@@ -27,65 +28,113 @@ async function fetchCategories(name) {
     return data
 }
 
-
 //* Création des éléments
-async function createElementByCat(categoryName, numberFilms) {
+async function createCarrouselByCat(categoryName, numberFilms) { // pas encore carrousel
     const catData = await fetchCategories(categoryName);
     const sectionCategories = document.getElementById("categories");
 
-    const newCategory = document.createElement('section');
-    newCategory.classList.add('category');
+    const category = document.createElement('section');
+    category.classList.add('category');
+    const carrousel = document.createElement('section');
+    carrousel.classList.add('container');
     const categoryTitle = document.createElement('h2');
     categoryTitle.innerHTML = categoryName;
+    const carrouselContent = document.createElement('div');
+    carrouselContent.classList.add('carrousel-content');
 
-    sectionCategories.appendChild(newCategory);
-    newCategory.appendChild(categoryTitle);
+    const navControl = document.createElement('div');
+    navControl.classList.add("nav-control");
+    const prevBtn = document.createElement('button');
+    prevBtn.classList.add('btn');
+    prevBtn.setAttribute("name", "previous");
+    prevBtn.setAttribute("id", "left");
+    prevBtn.setAttribute("onclick", "moveCarrouselBack");
+    prevBtn.innerHTML = "previous";
+    const nextBtn = document.createElement('button');
+    nextBtn.classList.add('btn');
+    nextBtn.setAttribute("name", "next");
+    nextBtn.setAttribute("id", "right");
+    nextBtn.setAttribute("onclick", "moveCarrouselForward");
+    nextBtn.innerHTML = "next";
+
+
+    navControl.appendChild(prevBtn);
+    navControl.appendChild(nextBtn);
+
+
+    sectionCategories.appendChild(category);
+    category.appendChild(categoryTitle);
+    category.appendChild(carrousel);
+    carrousel.appendChild(carrouselContent);
+    carrousel.appendChild(navControl);
 
     for (let i = 0; i < numberFilms; i++) {
 
         const dataDescription = await fetch(catData["results"][i]["url"]);
         const textDescription = await dataDescription.json();
 
-        const articleCategory = document.createElement('article');
+        const ficheFilm = document.createElement('article');
         const filmTitre = document.createElement('h2');
         const filmCover = document.createElement('img');
         const filmDescription = document.createElement('p');
-
+        const filmInfoBtn = document.createElement('button');
 
         filmTitre.innerText = catData["results"][i]["title"];
         filmDescription.innerHTML = textDescription["description"];
         filmCover.src = catData["results"][i]["image_url"];
+        filmInfoBtn.classList.add('modalBtn');
+        filmInfoBtn.setAttribute("onclick", `openModal()`);
+        filmInfoBtn.innerHTML = "more info";
 
 
-        newCategory.appendChild(articleCategory);
-        articleCategory.appendChild(filmTitre);
-        articleCategory.appendChild(filmDescription);
-        articleCategory.appendChild(filmCover);
+        carrouselContent.appendChild(ficheFilm);
+        ficheFilm.appendChild(filmTitre);
+        ficheFilm.appendChild(filmDescription);
+        ficheFilm.appendChild(filmCover);
+        ficheFilm.appendChild(filmInfoBtn);
 
     };
 }
 
+// controle du Carrousel
+function moveCarrouselBack() {
 
-
-
-
-
-
-
-
-//* exemple de modal
-function modal() {
-    el = document.getElementById("myModal");
-    el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
-let btn = document.getElementById("info-btn");
-btn.addEventListener("click", modal);
+
+function moveCarrouselForward() {
+
+}
+// modal
+function openModal() {   // ajouter un paramètre pour identifier le film en question
+    let modal = document.getElementById("modal");
+    let closeBtn = document.getElementsByClassName("close-btn")[0];
+
+    // ajouter fonction qui va récupérer les données du films et les assigner aux balises
+
+    modal.style.display = "block";
+
+    closeBtn.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    };
+}
+
+// function fetchDataModalFilm() { //ajouter paramètre pour identifier le film ?
+
+// }
+
+
 
 //* chargement de la page et màj des données
 
 window.addEventListener('load', () => {
     fetchBest()
-    createElementByCat("Adventure", 3)
-    createElementByCat("Animation", 3)
-    createElementByCat("Sport", 3)
+    createCarrouselByCat("Adventure", 3)
+    createCarrouselByCat("Animation", 3)
+    createCarrouselByCat("Sport", 3)
 });
